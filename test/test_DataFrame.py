61a2ff4efd96745c22d0f66b5dac87c909c2cb70
Stephan Schweitzer <steezefanschweitzer@yahoo.com>
2023-05-31 00:55:26 +0200

    functions for groupby method and basic concept of main groupby method
    -m "rebasing groupBy branch"

--- test/test_DataFrame.py
+++ test/test_DataFrame.py
@@ -7,3 +7,9 @@ class TestDataFrame(TestCase):
     def test_iloc(self):
         df_a = DataFrame(column=["a", "b", "c"], data=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 20]])
         self.assertEqual(df_a.iloc[0, 0], 1)
+
+    def test_groupby(self, column_to_group):
+        df_a = DataFrame(column=["Name", "Record", "Super Bowl Wins", "Uniform Coolness"],
+                         data=[["Cheifs", "7-7", 3, "OK"], ["Chargers", "0-14", 0, "Sick"], ["Broncos", "7-7", 3, "OK"],
+                               ["Bills", "7-7", 1, "OK"]])
+        self.assertEqual()
