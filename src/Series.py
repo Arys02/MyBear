28a866e752a29be5b5ae3ea8c2620c46ec792c5c
arys <charles.yaiche.lf@gmail.com>
2023-05-31 10:04:06 +0200

    [DOC] set some comment for series

--- src/Series.py
+++ src/Series.py
@@ -3,7 +3,28 @@ from math import sqrt
 
 
 class Series:
+    """
+       Une classe représentant une Série, similaire à une Série pandas.
+
+       Attributes
+       ----------
+       data : list
+           La liste de données de la Série.
+       name : str
+           Le nom de la Série.
+       size : int
+           La taille de la Série.
+       missing_values : int
+           Le nombre de valeurs manquantes dans la Série.
+       dtype : type, optional
+           Le type de données de la Série.
+       """
     def __init__(self, data, name, dtype=None, clone=False):
+        """
+                Initialise la Série avec des données, un nom et éventuellement un type de données.
+
+                Si clone est True, crée une copie des données. Sinon, utilise les données directement.
+                """
         self.data = data.copy() if clone else data
         self.name = name
         self.size = len(data)
@@ -16,12 +37,21 @@ class Series:
 
     @property
     def iloc(self):
+        """
+        Permet l'indexation basée sur la position des éléments dans la Série.
+        """
         return self
 
     def copy(self):
+        """
+        Permet l'indexation basée sur la position des éléments dans la Série.
+        """
         return Series(self.data, self.name, clone=True)
 
     def __getitem__(self, item):
+        """
+            Permet l'accès aux éléments de la Série par indexation.
+        """
         if isinstance(item, slice):
             return Series(self.data[item], self.name)
         return self.data[item]
